f37a5a767261c08ad1f9b59917945b73fdaefb9b
Craig Anthony <mcraiganthony@gmail.com>
2018-05-18 17:10:20 -0400

    Craig - more setup

--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,31 @@ var browserSync  = require('browser-sync'),
     notify       = require("gulp-notify"),
     plumber      = require('gulp-plumber'),
     uglify       = require('gulp-uglify'),
-    reload       = browserSync.reload;
+    reload       = browserSync.reload,
+    svgsprite    = require("gulp-svg-sprite");
 
 
 // Path Variables
 var paths =  {
   "html": {
-    "src_files": "dist/*.html"
-    "dist_dir": "dist/"
+    "src": "src/index.html",
+    "dest": "dist/"
   },
   "icons": {
-    "src_dir": "src/icons/**",
-    "dist_dir": "dist/css/icons/"
+    "src": "src/icons/**",
+    "dest": "dist/icons/"
   },
   "media": {
-    "src_dir": "src/media/**",
-    "dist_dir": "dist/media/"
+    "src": "src/media/**",
+    "dest": "dist/media/"
   },
   "styles": {
-    "src_files": "src/less/**/*.less",
-    "dist_dir": "dist/css/"
+    "src": "src/less/**/*.less",
+    "dest": "dist/css/"
   },
   "js": {
-    "src_files": "src/js/*.js",
-    "dist_dir": "dist/js/"
+    "src": "src/js/*.js",
+    "dest": "dist/js/"
   }
 };
 
@@ -43,8 +44,8 @@ var paths =  {
 gulp.task('styles', function() {
   return gulp.src(["src/less/app.less"])
     .pipe(less({ compress: true }))
-    .pipe(autoprefixer({ browsers: ['last 2 versions','ie 9'], cascade: false }))
-    .pipe(gulp.dest(paths.styles.dist_dir))
+    .pipe(autoprefixer({ browsers: ['last 2 versions','ie 10'], cascade: false }))
+    .pipe(gulp.dest(paths.styles.dest))
     .pipe(duration("building styles"))
     .pipe(notify({ message: "styles task complete" }))
     .pipe(reload({stream:true}));
@@ -54,15 +55,14 @@ gulp.task('styles', function() {
 // Javascript
 gulp.task('js', function() {
   return gulp.src([
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/bootstrap/js/tooltip.js',
-    'bower_components/scrollReveal.js/dist/scrollReveal.min.js',
-    'bower_components/smoothScroll.js/smoothScroll.js',
-    paths.js.src_files
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/scrollreveal/dist/scrollreveal.min.js',
+    'node_modules/smoothscroll/smoothscroll.min.js',
+    paths.js.src
     ])
     .pipe(concat('app.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.js.dist_dir))
+    .pipe(gulp.dest(paths.js.dest))
     .pipe(duration("building js"))
     .pipe(notify({ message: "js task complete" }))
     .pipe(reload({stream:true}));
@@ -71,32 +71,70 @@ gulp.task('js', function() {
 
 // HTML
 gulp.task('html', function() {
-  return gulp.src([paths.html.src_files])
-    .pipe(reload({stream:true}));
+  return gulp.src([paths.html.src])
+    .pipe(reload({stream:true}))
+    .pipe(gulp.dest(paths.html.dest));
 });
 
 // Icons
-gulp.task('icons', function() {
-  return gulp.src([paths.icons.src_dir])
-    .pipe(changed(paths.icons.dist_dir))
-    .pipe(gulp.dest(paths.icons.dist_dir))
+/*gulp.task('icons', function() {
+  const path = paths.icons;
+  return gulp.src([paths.icons.src])
+    .pipe(changed(paths.icons.dest))
+    .pipe(gulp.dest(paths.icons.dest))
     .pipe(reload({stream:true}));
+});*/
+
+
+gulp.task('icons', function() {
+
+  return gulp.src([paths.icons.src])
+    .pipe(svgsprite({
+      shape: {
+        id: {
+          generator: 'icon-',
+        },
+        dimension: {
+          attributes: false,
+        },
+      },
+      mode: {
+        symbol: {
+          dest: '',
+          example: false,
+          sprite: 'sprite.svg',
+        },
+      },
+      svg: {
+        xmlDeclaration: false,
+        doctypeDeclaration: false,
+        dimensionAttributes: false,
+        rootAttributes: {
+          style: 'border: 0; clip: rect(0 0 0 0); height: 0; overflow: hidden; padding: 0; position: absolute; width: 0;',
+        },
+      },
+    }))
+    //.pipe(gulp.dest(paths.icons.dest))
+    .pipe(duration('Built Icons'))
+    .pipe(reload({ stream: true }));
 });
+
+
 
 // Images
 gulp.task('media', function() {
-  return gulp.src([paths.media.src_dir])
-    .pipe(changed(paths.media.dist_dir))
-    .pipe(gulp.dest(paths.media.dist_dir))
+  return gulp.src([paths.media.src])
+    .pipe(changed(paths.media.dest))
+    .pipe(gulp.dest(paths.media.dest))
     .pipe(reload({stream:true}));
 });
 
 
 // Watch
 gulp.task("watch", function() {
-  gulp.watch(paths.styles.src_files, ["styles"]);
-  gulp.watch(paths.js.src_files, ["js"]);
-  gulp.watch(paths.html.src_files, ["html"]);
+  gulp.watch(paths.styles.src, ["styles"]);
+  gulp.watch(paths.js.src, ["js"]);
+  gulp.watch(paths.html.src, ["html"]);
 });
 
 
@@ -123,5 +161,5 @@ gulp.task('default', ['styles','js','media','icons']);
 
 // Gulp Server
 gulp.task('server', ['default', 'watch', 'browser-sync'], function () {
-    gulp.watch([paths.html.src_files], reload);
+    gulp.watch([paths.html.src], reload);
 });
