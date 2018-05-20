b09bb177df83feeccb8be6b8b3b1bf3a17b73fb4
Craig Anthony <mcraiganthony@gmail.com>
2018-05-20 13:32:44 -0400

    Craig - favicon, cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,6 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
   return gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/scrollreveal/dist/scrollreveal.min.js',
-    'node_modules/smoothscroll/smoothscroll.min.js',
     paths.js.src
     ])
     .pipe(concat('app.js'))
