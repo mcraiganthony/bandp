0d622a27874a2e480601194313411cd1b26531aa
Craig Anthony <mcraiganthony@gmail.com>
2018-05-19 21:00:21 -0400

    Craig - svg loading, icons

--- src/js/svgloader.js
+++
@@ -1,5 +0,0 @@
-$.get("icons/sprite.svg", function(data) {
-  var div = document.createElement("div");
-  div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
-  document.body.insertBefore(div, document.body.childNodes[0]);
-});
